fd9799ee362b713b51978d81f64c89fe7db454d8
Nikita Mogilevsky <nikitamog@gmail.com>
2019-10-03 10:20:47 -0700

    I don't remember what I changed

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -4,7 +4,7 @@
   @email: nikitamog@bellevuecollege.edu
   @description:
   demonstrate some properties of linked lists.
- */
+*/
 
 #include <stdexcept>
 #include "LinkedLists.h"
@@ -16,17 +16,17 @@ template <typename T>
 Node<T>::Node(Node<T>& p,
               T& d,
               Node<T>& n)
-  :prev{p},data{d},next{n}{}
+    :prev{p},data{d},next{n}{}
 
 template <typename T>
 Node<T>::Node(T& d) {
-  Node(NULL,d,NULL);
+    Node(NULL,d,NULL);
 }
 
 template <typename T>
 Node<T>::~Node<T>()
 {
-  delete data;
+    delete data;
 }
 //============================================
 // operators
@@ -34,62 +34,64 @@ Node<T>::~Node<T>()
 template <typename T>
 Node<T> Node<T>::operator=(Node<T>& n)
 {
-  this->data = n->data;
-  return *this;
+    this->data = n->data;
+    return *this;
 }
 
 // arithmetic
 template <typename T>
 Node<T> Node<T>::operator+(Node<T>& n)
 {
-  this->data = this->data + n->data;
-  return *this;
+    this->data = this->data + n->data;
+    return *this;
 }
 
 template <typename T>
 Node<T> Node<T>::operator-(Node<T>& n)
 {
-  this->data = this->data - n->data;
-  return *this;
+    this->data = this->data - n->data;
+    return *this;
 }
 
 template <typename T>
 Node<T> Node<T>::operator*(Node<T>& n)
 {
-  this->data = this->data * n->data;
-  return *this;
+    this->data = this->data * n->data;
+    return *this;
 }
 
 template <typename T>
 Node<T> Node<T>::operator/(Node<T>& n)
 {
-  this->data = this->data / n->data;
-  return *this;
+    this->data = this->data / n->data;
+    return *this;
 }
 
 template <typename T>
 Node<T> Node<T>::operator%(Node<T>& n)
 {
-  this->data = this->data % n->data;
-  return *this;
+    this->data = this->data % n->data;
+    return *this;
 }
 //============================================
-// Linked List
+//============================================
+// CLASS: Linked List
+//============================================
 //============================================
 
 template <typename T>
 LinkedList<T>::~LinkedList() //destructor
 {
-  if (head == NULL) return;
-  Node<T>* curr = head;
-  Node<T>* next = curr->next;
-  while(next != NULL)
+    if (head == NULL) return;
+    Node<T>* curr = head;
+    Node<T>* next = curr->next;
+    while(next != NULL)
     {
-      delete *curr;
-      curr = next;
-      next = curr->next;
+        delete *curr;
+        curr = next;
+        next = curr->next;
     }
-  delete curr;
+    delete curr;
 }
 //============================================
 // operators
@@ -98,18 +100,18 @@ LinkedList<T>::~LinkedList() //destructor
 template <typename T>
 Node<T> LinkedList<T>::operator[](int index) const
 {
-  int i = index;
-  // check lower bound
-  if (i < 0) throw out_of_range("Linked List");  
-  Node<T>* curr = head;
-  while (curr != NULL && i > 0)
+    int i = index;
+    // check lower bound
+    if (i < 0) throw out_of_range("Linked List");  
+    Node<T>* curr = head;
+    while (curr != NULL && i > 0)
     {
-      curr = curr.next;
-      --i;
+        curr = curr.next;
+        --i;
     }  
-  if (i > 0) throw out_of_range("Linked List");
-  // check upper bound
-  return *curr;
+    if (i > 0) throw out_of_range("Linked List");
+    // check upper bound
+    return *curr;
 }
 //============================================  
 // methods
@@ -118,71 +120,86 @@ Node<T> LinkedList<T>::operator[](int index) const
 template <typename T>
 void LinkedList<T>::remove(int index)
 {
-  Node<T>* curr = &this[index];
-  curr->prev->next = curr->next;
-  curr->next->prev = curr->prev;
+    Node<T>* curr = &this[index];
+    curr->prev->next = curr->next;
+    curr->next->prev = curr->prev;
 }
 
 template <typename T>
 void LinkedList<T>::add(T* data, int index)
 {
-  // needs head and tail updates
-  Node<T>* curr = &this[index];
-  Node<T>* newnode = new Node<T>(curr, data, curr->next);
-  curr->next = newnode;
+    // check append
+    if (index == size)
+    {
+        add(data);
+        return;
+    }
+    Node<T>* curr = &this[index];
+    // check prepend.
+    if (index == 0) head = curr;
+    Node<T>* newnode = new Node<T>(curr,
+                                   data,
+                                   curr->next);
+    curr->next = newnode;
 }
 
 template <typename T>
 void LinkedList<T>::add(T* data)
 {
-  // last element
-  Node<T>* newnode = new Node<T>(tail, data, NULL);
-  if(head==NULL) head = newnode;
-  tail = newnode;
+    // last element
+    Node<T>* newnode = new Node<T>(tail,
+                                   data, NULL);
+    if(head==NULL) head = newnode;
+    tail = newnode;
 }
 
 template <typename T>
 bool LinkedList<T>::isEmpty() const
 {
-  return head == NULL;
+    return head == NULL;
 }
 
 template <typename T>
 int LinkedList<T>::length() const
 {
-  int i = 0;
-  Node<T>* curr = head;
-  while(curr != NULL)
+    int i = 0;
+    Node<T>* curr = head;
+    while(curr != NULL)
     {
-      curr = curr->next;
-      ++i;
+        curr = curr->next;
+        ++i;
     }
-  return i;
+    return i;
 }
 
 template <typename T>
 T* LinkedList<T>::front() const
 {
-  assert(head != NULL);
-  return head->data;
+    assert(head != NULL);
+    return head->data;
 }
 
 template <typename T>
 T* LinkedList<T>::last() const
 {
-  assert(head != NULL);
-  return tail->data;
+    assert(head != NULL);
+    return tail->data;
 }
 
 template <typename T>
 void LinkedList<T>::insertFirst(T* data)
 {
-  this->add(data, 0);
+    this->add(data, 0);
 }
 
 template <typename T>
 void LinkedList<T>::insertLast(T* data)
 {
-  this->add(data);
+    this->add(data);
 }
   
+template <typename T>
+int LinkedList<T>::size()
+{
+    return size;
+}
--- LinkedLists.h
+++ LinkedLists.h
@@ -5,8 +5,8 @@
   @author: Nikita Mogilevsky
   @email: nikitamog@bellevuecollege.edu
   @description:
-* Stuff todo
-** TODO update tail references in add and remove.
+  * Stuff todo
+  ** TODO update tail references in add and remove.
   */
 
 #include <stdexcept>
@@ -16,45 +16,46 @@ using namespace std;
 template <typename T>
 struct Node
 {
-  Node<T>* prev;
-  Node<T>* next;
-  // some generic type for data.
-  T* data;
+    Node<T>* prev;
+    Node<T>* next;
+    // some generic type for data.
+    T* data;
   
-  // for sorting methods, maybe.
-  Node<T> operator=(Node<T>& n);
-  Node<T> operator+(Node<T>& n);
-  Node<T> operator-(Node<T>& n);
-  Node<T> operator*(Node<T>& n);
-  Node<T> operator/(Node<T>& n);
-  Node<T> operator%(Node<T>& n);
+    // for sorting methods, maybe.
+    Node<T> operator=(Node<T>& n);
+    Node<T> operator+(Node<T>& n);
+    Node<T> operator-(Node<T>& n);
+    Node<T> operator*(Node<T>& n);
+    Node<T> operator/(Node<T>& n);
+    Node<T> operator%(Node<T>& n);
 
-  Node<T>(T& d); // d for data
-  Node<T>(Node<T>& prev, T& data, Node<T>& next);
-  ~Node<T>();
+    Node<T>(T& d); // d for data
+    Node<T>(Node<T>& prev, T& data, Node<T>& next);
+    ~Node<T>();
 }; //Node
 
 template <typename T>
 class LinkedList // singley linked
 {
-  // private by default.
-  Node<T>* head;
-  Node<T>* tail;
-  int size; // update length
+    // private by default.
+    Node<T>* head;
+    Node<T>* tail;
+    int size; // update length
 
 public:
-  ~LinkedList(); //destructor
+    ~LinkedList(); //destructor
 
-  bool isEmpty() const;
-  int length() const;
-  T* front() const;
-  T* last() const;
-  void remove(int index);
-  void add(T* data, int index);
-  void add(T* data);
-  void insertFirst(T* data); // TODO
-  void insertLast(T* data); // TODO
-  Node<T> operator[](int index) const;  
+    bool isEmpty() const;
+    int length() const;
+    T* front() const;
+    T* last() const;
+    int size() const;       
+    void remove(int index);
+    void add(T* data, int index);
+    void add(T* data); // appends data
+    void insertFirst(T* data); // TODO
+    void insertLast(T* data); // TODO
+    Node<T> operator[](int index) const;  
   
 }; // Linked List
 
